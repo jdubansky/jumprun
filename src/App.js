b15a8bf865d8cdec33616f5052da635fc4baacb3
Jeremy Dubansky <jdubansky@gmail.com>
2022-08-20 14:49:56 -0500

    latest

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import axios from 'axios';
 
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+//{"lat": 1.606518333, "lon": -3.85963, "track": 159.96, "magtrack": 160.726, "magvar": 0.8, "speed": 0.797, "climb": 0.1}
 function App() {
+  let [lat, setLat] = React.useState(0.0);
+  let [lon, setLon] = React.useState(0.0);
+  let [track, setTrack] = React.useState(0.0);
+  let [speed, setSpeed] = React.useState(0.0);
+
+  function fetchDataFromApi(){
+    axios.get(`https://630110489a1035c7f8fd5aaf.mockapi.io/data`)
+      .then(res => {
+        console.log(res.data);
+        setLat(res.data[0]['lat']);
+        setTrack(res.data[0]['track']);
+        setSpeed(res.data[0]['speed']);
+      })
+  }
+
+  React.useEffect(() => {
+    fetchDataFromApi();
+    setInterval(fetchDataFromApi(), 30000);
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section className="py-5"><div className="container">
+  <div className="row pt-2">
+    <div className="col-lg-6 mt-3">
+      <h2>Direction</h2>
+      <h2> {track} </h2>
+    </div>
+    <div className="col-lg-6 mt-3">
+      <h2>NORTHWEST</h2>
+    </div>
+    <div className="col-lg-6 mt-3">
+      <h2>Airspeed</h2>
+      <h2> {speed} </h2>
+    </div>
+    <div className="col-lg-6 mt-3">
+      <h2>Seperation</h2>
+      <h2>99 Seconds</h2>
+    </div>
+  </div>
+  </div>
+</section>
     </div>
   );
 }
